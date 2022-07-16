88182ca74683fa27379f4128e501b25927193ba4
Oscar marina <oscar7600@outlook.com>
2022-07-17 01:01:45 +0200

    refactor: clean generate site

--- scripts/move-dev-directory.js
+++ scripts/move-dev-directory.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { readdirSync, statSync, readFileSync, writeFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname, resolve, join, sep, basename } from 'path';
+import { dirname, resolve, join, sep } from 'path';
 import copy from 'recursive-copy';
 import * as cheerio from 'cheerio';
 
@@ -45,18 +45,10 @@ const getAllDev = (
 
 getAllDev().forEach((dev) => {
   copy(dev.devDirectory, resolve(`./site/${dev.devDirectoryParent}`))
-    /* .on(copy.events.COPY_FILE_COMPLETE, (copyOperation) => {
-      if (basename(copyOperation.dest) === 'index.html') {
-        const $html = cheerio.load(readFileSync(copyOperation.dest));
-        $html('title').after(`<base href="${dev.devDirectoryParent}/">`);
-        writeFileSync(copyOperation.dest, $html.html(), 'utf8');
-      }
-    }) */
     .then((results) => {
       const $olNode = $indexHtml('ol').append(
         `<li><a href="${dev.devDirectoryParent}/index.html">${dev.devDirectoryParent}</a></li>`
       );
-
       writeFileSync(resolve(`./site/index.html`), $indexHtml.html(), 'utf8');
       console.info(
         `${results.length} file(s) copied - ${dev.devDirectoryParent}`
