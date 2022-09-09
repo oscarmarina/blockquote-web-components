581b0c85707099e3fdb6e1fe523a3ebf2fbd6fd4
oscar marina <oscar7600@outlook.com>
2022-09-09 18:01:20 +0200

    fix(blockquote-base-meta): update demo

--- packages/base/blockquote-base-meta/README.js
+++ packages/base/blockquote-base-meta/README.js
@@ -1,5 +1,4 @@
-# blockquote-base-meta
-
+/**
 ![Lit](https://img.shields.io/badge/lit-2.0.0-blue)
 
 `BlockquoteBaseMeta` is based on Polymer's `iron-meta`, and it is a generic class that you can use
@@ -20,10 +19,10 @@ But the main difference is that Map allows keys of any type.
 ```js
 import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-meta';
 
- const myDefault = new BlockquoteBaseMeta({
-   key: 'basic',
-   value: 'foo/bar',
- });
+  const myDefault = new BlockquoteBaseMeta({
+    key: 'basic',
+    value: 'foo/bar',
+  });
 
 console.log(myDefault.value); // foo/bar
 ```
@@ -33,13 +32,13 @@ console.log(myDefault.value); // foo/bar
 ```js
 import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-meta';
 
- const myDefault = new BlockquoteBaseMeta({
-   type: 'one',
-   key: 'basic',
-   value: 'foo/bar',
- });
+  const myDefault = new BlockquoteBaseMeta({
+    type: 'one',
+    key: 'basic',
+    value: 'foo/bar',
+  });
 
- console.log(myDefault.objectList); // {basic: 'foo/bar'}
+  console.log(myDefault.objectList); // {basic: 'foo/bar'}
 ```
 
 ## Keys any type - Map
@@ -47,16 +46,21 @@ import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-m
 ```js
 import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-meta';
 
- const keyInfo = { id: 'dsfaskj0' };
- const myDefault = new BlockquoteBaseMeta({
-   type: 'two',
-   key: keyInfo,
-   value: 'foo/bar',
- });
+  const keyInfo = { id: 'dsfaskj0' };
+  const myDefault = new BlockquoteBaseMeta({
+    type: 'two',
+    key: keyInfo,
+    value: 'foo/bar',
+  });
 
 console.log(myDefault.mapList); // {{ id: 'dsfaskj0' }: 'foo/bar'}
 ```
 
 # ## Exports
 
- - BlockquoteBaseMeta
+  - BlockquoteBaseMeta
+
+@tagname blockquote-base-meta
+@element blockquote-base-meta
+*/
+export class ReadmElement extends HTMLElement {}
