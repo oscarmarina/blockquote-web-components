39ce9688baadf7966da3820e5479512450f14f6c
oscar marina <oscar7600@outlook.com>
2022-09-12 22:59:58 +0200

    fix(blockquote-base-style-helpers): remove minify html literals

--- packages/base/blockquote-base-style-helpers/demo/theme/theme.js
+++ packages/base/blockquote-base-style-helpers/demo/theme/theme.js
@@ -13,30 +13,35 @@ const theme = {
     ...fontFace,
   },
   fonts: {
-    main: 'Kaisei HarunoUmi, sans-serif',
+    main: 'Kaisei HarunoUmi, serif',
   },
 };
 
+const THEME = `
+${theme.fontFace.main}
+:root {
+  --red-300: ${theme.colors['--red-300']};
+  --red-400: ${theme.colors['--red-400']};
+  --red-500: ${theme.colors['--red-500']};
+  --red-600: ${theme.colors['--red-600']};
+  --red-700: ${theme.colors['--red-700']};
+
+  --green-300: ${theme.colors['--green-300']};
+  --green-400: ${theme.colors['--green-400']};
+  --green-500: ${theme.colors['--green-500']};
+  --green-600: ${theme.colors['--green-600']};
+  --green-700: ${theme.colors['--green-700']};
+
+  --blue-300: ${theme.colors['--blue-300']};
+  --blue-400: ${theme.colors['--blue-400']};
+  --blue-500: ${theme.colors['--blue-500']};
+  --blue-600: ${theme.colors['--blue-600']};
+  --blue-700: ${theme.colors['--blue-700']};
+
+  font: normal medium/1.25 ${theme.fonts.main};
+
+}`;
+
 setDocumentStyles(css`
-  ${unsafeCSS(theme.fontFace.main)}
-  :root {
-    font: normal medium/1.25 ${unsafeCSS(theme.fonts.main)};
-    --red-300: ${unsafeCSS(theme.colors['--red-300'])};
-    --red-400: ${unsafeCSS(theme.colors['--red-400'])};
-    --red-500: ${unsafeCSS(theme.colors['--red-500'])};
-    --red-600: ${unsafeCSS(theme.colors['--red-600'])};
-    --red-700: ${unsafeCSS(theme.colors['--red-700'])};
-
-    --green-300: ${unsafeCSS(theme.colors['--green-300'])};
-    --green-400: ${unsafeCSS(theme.colors['--green-400'])};
-    --green-500: ${unsafeCSS(theme.colors['--green-500'])};
-    --green-600: ${unsafeCSS(theme.colors['--green-600'])};
-    --green-700: ${unsafeCSS(theme.colors['--green-700'])};
-
-    --blue-300: ${unsafeCSS(theme.colors['--blue-300'])};
-    --blue-400: ${unsafeCSS(theme.colors['--blue-400'])};
-    --blue-500: ${unsafeCSS(theme.colors['--blue-500'])};
-    --blue-600: ${unsafeCSS(theme.colors['--blue-600'])};
-    --blue-700: ${unsafeCSS(theme.colors['--blue-700'])};
-  }
+  ${unsafeCSS(THEME)}
 `);
