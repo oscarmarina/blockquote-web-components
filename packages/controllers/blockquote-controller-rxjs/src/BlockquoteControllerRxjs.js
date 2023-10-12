cfe4b79867aaba59f3d34e5fa68e645ec841016c
oscar marina <oscar7600@outlook.com>
2023-10-12 11:16:06 +0200

    refactor(blockquote-controller-rxjs): improve documentation and types

--- packages/controllers/blockquote-controller-rxjs/src/BlockquoteControllerRxjs.js
+++ packages/controllers/blockquote-controller-rxjs/src/BlockquoteControllerRxjs.js
@@ -4,72 +4,70 @@ const unsubscribe = Symbol('unsubscribe');
 const subscriptions = Symbol('subscriptions');
 
 /**
-![Lit](https://img.shields.io/badge/lit-2.0.0-blue)
-
-`BlockquoteControllerRxjs` is a Reactive Controller.
-
-- Original idea by [Adrian Fâciu](https://github.com/adrianfaciu/rx-lit)
-- [observables-litelement](https://adrianfaciu.dev/posts/observables-litelement/)
-
-`BlockquoteControllerRxjs` provides a subscribe method to which pass the property we want to
-assign values to and the Observable we want to subscribe.
-
-- It works with [reactive properties](https://lit.dev/docs/components/properties/) and no-reactive properties
-- It ignores calls on the same property with the same Observable
-- It unsubscribes from old observable if called again on the same property with different Observable
-- It unsubscribes when the component is removed
-
-## Usage
-
-```js
-class BlockquoteControllerRxjsDemo extends LitElement {
-  static get is() {
-    return 'blockquote-controller-rxjs-demo';
-  }
-
-  static get properties() {
-    return {
-      _pos: {
-        type: Object,
-        attribute: false,
-      },
-    };
-  }
-
-  constructor() {
-    super();
-    this.rx = new BlockquoteControllerRxjs(this);
-    this._pos = { x: 0, y: 0 };
-    this.values$ = fromEvent(window, 'mousemove').pipe(
-      map(({ clientX, clientY }) => ({ x: clientX, y: clientY })),
-    );
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-
-    // Property and Observable.
-    this.rx.subscribe('_pos', this.values$);
-  }
-
-  render() {
-    return html`
-      <p>The mouse is at:</p>
-      <pre>
-        x: ${this._pos.x}
-        y: ${this._pos.y}
-      </pre
-      >
-    `;
-  }
-}
-```
-
-  ## Exports
-
-  - BlockquoteControllerRxjs
-
-*/
+ * # BlockquoteControllerRxjs
+ *
+ * ![Lit](https://img.shields.io/badge/lit-2.0.0-blue)
+ *
+ * `BlockquoteControllerRxjs` is a Reactive Controller.
+ *
+ * - Original idea by [Adrian Fâciu](https://github.com/adrianfaciu/rx-lit)
+ * - [observables-litelement](https://adrianfaciu.dev/posts/observables-litelement/)
+ *
+ * `BlockquoteControllerRxjs` provides a subscribe method to which pass the property we want to
+ * assign values to and the Observable we want to subscribe.
+ *
+ * - It works with [reactive properties](https://lit.dev/docs/components/properties/) and non-reactive properties
+ * - It ignores calls on the same property with the same Observable
+ * - It unsubscribes from the old observable if called again on the same property with a different Observable
+ * - It unsubscribes when the component is removed
+ *
+ * ## Usage
+ *
+ * ```js
+ * class BlockquoteControllerRxjsDemo extends LitElement {
+ *   static get is() {
+ *     return 'blockquote-controller-rxjs-demo';
+ *   }
+ *
+ *   static get properties() {
+ *     return {
+ *       _pos: {
+ *         type: Object,
+ *         attribute: false,
+ *       },
+ *     };
+ *   }
+ *
+ *   constructor() {
+ *     super();
+ *     this.rx = new BlockquoteControllerRxjs(this);
+ *     this._pos = { x: 0, y: 0 };
+ *     this.values$ = fromEvent(window, 'mousemove').pipe(
+ *       map(({ clientX, clientY }) => ({ x: clientX, y: clientY })),
+ *     );
+ *   }
+ *
+ *   connectedCallback() {
+ *     super.connectedCallback();
+ *
+ *     // Property and Observable.
+ *     this.rx.subscribe('_pos', this.values$);
+ *   }
+ *
+ *   render() {
+ *     return html`
+ *       <p>The mouse is at:</p>
+ *       <pre>
+ *         x: ${this._pos.x}
+ *         y: ${this._pos.y}
+ *       </pre
+ *       >
+ *     `;
+ *   }
+ * }
+ * ```
+ *
+ */
 export class BlockquoteControllerRxjs {
   constructor(host) {
     this[unsubscribe] = new Subject();
@@ -81,7 +79,9 @@ export class BlockquoteControllerRxjs {
     if (!isObservable(stream$)) {
       throw new Error('Invalid Observable!');
     }
+
     const existingSubscription = this[subscriptions].get(propKey);
+
     if (existingSubscription) {
       if (existingSubscription?.stream$ === stream$) {
         return stream$;
@@ -89,9 +89,7 @@ export class BlockquoteControllerRxjs {
       existingSubscription?.subscription?.unsubscribe();
     }
 
-    // eslint-disable-next-line arrow-parens
     const subscription = stream$.pipe(takeUntil(this[unsubscribe])).subscribe(state => {
-      // eslint-disable-next-line no-unused-expressions
       propKey in this.host && (this.host[propKey] = state);
       this.host.requestUpdate();
     });
--- packages/controllers/blockquote-controller-rxjs/test/blockquote-controller-rxjs.test.js
+++ packages/controllers/blockquote-controller-rxjs/test/blockquote-controller-rxjs.test.js
@@ -4,43 +4,45 @@ import { from, Subject } from 'rxjs';
 import { LitElement } from 'lit';
 import { BlockquoteControllerRxjs } from '../index.js';
 
-window.customElements.define(
-  'blockquote-controller-rxjs-demo',
-  class BlockquoteControllerRxjsDemo extends LitElement {
-    static get properties() {
-      return {
-        streamValues: {
-          type: Number,
-          attribute: false,
-        },
-      };
-    }
+const RxjsDemo = class BlockquoteControllerRxjsDemo extends LitElement {
+  static get properties() {
+    return {
+      streamValues: {
+        type: Number,
+        attribute: false,
+      },
+    };
+  }
 
-    constructor() {
-      super();
-      this.rx = new BlockquoteControllerRxjs(this);
-      this.streamValues = '';
-      this.streamValuesUpdate = '';
-    }
+  constructor() {
+    super();
+    this.rx = new BlockquoteControllerRxjs(this);
+    this.streamValues = 0;
+    this.streamValuesUpdate = 0;
+  }
 
-    setupObservable(stream$) {
-      this.rx.subscribe('streamValues', stream$);
-    }
+  setupObservable(stream$) {
+    this.rx.subscribe('streamValues', stream$);
+  }
 
-    setupObservableNeedUpdate(stream$) {
-      this.rx.subscribe('streamValuesUpdate', stream$);
-    }
+  setupObservableNeedUpdate(stream$) {
+    this.rx.subscribe('streamValuesUpdate', stream$);
+  }
 
-    render() {
-      return html`
-        <b>${this.streamValues}</b>
-        <i>${this.streamValuesUpdate}</i>
-      `;
-    }
-  },
-);
+  render() {
+    return html`
+      <b>${this.streamValues}</b>
+      <i>${this.streamValuesUpdate}</i>
+    `;
+  }
+};
+
+window.customElements.define('blockquote-controller-rxjs-demo', RxjsDemo);
 
 suite('BlockquoteControllerRxjs', () => {
+  /**
+   * @type {RxjsDemo}
+   */
   let el;
 
   teardown(() => fixtureCleanup());
@@ -54,19 +56,19 @@ suite('BlockquoteControllerRxjs', () => {
     });
 
     test('can handle simple Observable - with Reactive property', async () => {
-      const nodeText = el.shadowRoot.querySelector('b');
+      const nodeText = el.shadowRoot?.querySelector('b');
       el.setupObservable(from([1]));
       assert.equal(el.streamValues, 1);
       await el.updateComplete;
-      assert.equal(nodeText.textContent, 1);
+      assert.equal(nodeText?.textContent, '1');
     });
 
     test('can handle simple Observable - without Reactive property', async () => {
-      const nodeText = el.shadowRoot.querySelector('i');
+      const nodeText = el.shadowRoot?.querySelector('i');
       el.setupObservableNeedUpdate(from([2]));
       assert.equal(el.streamValuesUpdate, 2);
       await el.updateComplete;
-      assert.equal(nodeText.textContent, 2);
+      assert.equal(nodeText?.textContent, '2');
     });
 
     test('can handle invalid input', () => {
@@ -78,8 +80,10 @@ suite('BlockquoteControllerRxjs', () => {
       el.setupObservable(stream$);
       stream$.next(1);
       assert.equal(el.streamValues, 1);
+      // @ts-ignore
       assert.equal(stream$.currentObservers.size, 1);
       el.remove();
+      // @ts-ignore
       assert.equal(stream$.currentObservers.size, 0);
     });
 
@@ -88,8 +92,10 @@ suite('BlockquoteControllerRxjs', () => {
       el.setupObservable(stream$);
       stream$.next(1);
       assert.equal(el.streamValues, 1);
+      // @ts-ignore
       assert.equal(stream$.currentObservers.size, 1);
       el.setupObservable(new Subject());
+      // @ts-ignore
       assert.equal(stream$.currentObservers.size, 0);
     });
 
@@ -98,8 +104,10 @@ suite('BlockquoteControllerRxjs', () => {
       el.setupObservable(stream$);
       stream$.next(1);
       assert.equal(el.streamValues, 1);
+      // @ts-ignore
       assert.equal(stream$.currentObservers.size, 1);
       el.setupObservable(stream$);
+      // @ts-ignore
       assert.equal(stream$.currentObservers.size, 1);
     });
   });
