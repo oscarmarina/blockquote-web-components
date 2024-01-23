6d436ff746eabafc0173aab7b624202f1a79ad0d
oscar marina <oscar7600@outlook.com>
2024-01-23 21:45:33 +0100

    feat(blockquote-base-meta): add globalThis

--- packages/base/blockquote-base-meta/src/BlockquoteBaseMeta.js
+++ packages/base/blockquote-base-meta/src/BlockquoteBaseMeta.js
@@ -9,19 +9,21 @@ part of the polymer project is also subject to an additional IP rights grant
 found at http://polymer.github.io/PATENTS.txt
 */
 
+const globalThisOrWindow = globalThis /* c8 ignore next */ || window;
+
 const BLOCKQUOTE = Symbol.for('BLOCKQUOTE');
 const blockquoteBaseMeta = Symbol.for('blockquoteBaseMeta');
 const types = Symbol.for('types');
 const uuid = Symbol.for('uuid');
 
-const blockquote = window[BLOCKQUOTE] || Object.create(null);
+const blockquote = globalThisOrWindow[BLOCKQUOTE] || Object.create(null);
 const baseMeta = blockquote[blockquoteBaseMeta] || Object.create(null);
 
 baseMeta[types] = baseMeta[types] || new Map();
 baseMeta[uuid] = baseMeta[uuid] || Math.random().toString(36).substring(2, 10);
 
 blockquote[blockquoteBaseMeta] = baseMeta;
-window[BLOCKQUOTE] = blockquote;
+globalThisOrWindow[BLOCKQUOTE] = blockquote;
 
 // https://www.oreilly.com/library/view/learning-javascript-design/9781449334840/ch13s15.html
 // https://www.keithcirkel.co.uk/metaprogramming-in-es6-symbols/
@@ -29,61 +31,86 @@ window[BLOCKQUOTE] = blockquote;
 /**
  * ![Lit](https://img.shields.io/badge/lit-3.0.0-blue.svg)
  *
- * `BlockquoteBaseMeta` is based on Polymer's `iron-meta`, and it is a generic class that you can use for sharing information across the DOM tree.
- * It uses [monostate pattern](http://c2.com/cgi/wiki?MonostatePattern) pattern such that any instance of it has access to the shared information.
- * You can use `BlockquoteBaseMeta` to share whatever you want.
- * The `BlockquoteBaseMeta` instances contain your actual data. The only requirement is that you
- * create them before you try to access them.
+ * `BlockquoteBaseMeta` is a generic class for sharing information across the DOM tree, inspired by Polymer's [`iron-meta`](https://github.com/PolymerElements/iron-meta/blob/master/iron-meta.js).
+ * It employs the [monostate pattern](http://c2.com/cgi/wiki?MonostatePattern) to allow any instance to access the shared information.
  *
- * `BlockquoteBaseMeta` uses [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map).
+ * The class instances hold the actual data. They must be created before attempting to access them.
  *
- * Map is a collection of keyed data items, just like an Object.
- * But the main difference is that Map allows keys of any type.
+ * `BlockquoteBaseMeta` uses a [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) to store the data. Unlike an Object, a Map allows keys of any type.
  *
  * ### Demo
  *
  * [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/oscarmarina/blockquote-web-components/tree/main/packages/base/blockquote-base-meta)
  *
- * ### Usage
+ * #### Usage
  *
  * ```js
  * import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-meta';
  *
- *   const myDefault = new BlockquoteBaseMeta({
- *     key: 'basic',
- *     value: 'foo/bar',
- *   });
+ * const myDefault = new BlockquoteBaseMeta({
+ *   key: 'basic',
+ *   value: 'foo/bar',
+ * });
+ *
+ * console.log(myDefault.byKey('basic')); // foo/bar
+ * ```
+ *
+ * #### Value Getter - String
+ *
+ * ```js
+ * import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-meta';
+ *
+ * const myDefault = new BlockquoteBaseMeta({
+ *   key: 'basic',
+ *   value: 'foo/bar',
+ * });
  *
  * console.log(myDefault.value); // foo/bar
  * ```
  *
- * ### Keys string - Object
+ * #### List Getter - Array
+ *
+ * The `list` getter returns an array of all values stored in the `BlockquoteBaseMeta` instance.
  *
  * ```js
  * import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-meta';
  *
- *   const myDefault = new BlockquoteBaseMeta({
- *     type: 'one',
- *     key: 'basic',
- *     value: 'foo/bar',
- *   });
+ * const myDefault = new BlockquoteBaseMeta({
+ *   type: 'three',
+ *   key: 'basic',
+ *   value: 'foo/bar',
+ * });
  *
- *   console.log(myDefault.objectList); // {basic: 'foo/bar'}
+ * console.log(myDefault.list); // ['foo/bar']
  * ```
  *
- * ### Keys any type - Map
+ * #### ObjectList Getter - Object
  *
  * ```js
  * import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-meta';
  *
- *   const keyInfo = { id: 'dsfaskj0' };
- *   const myDefault = new BlockquoteBaseMeta({
- *     type: 'two',
- *     key: keyInfo,
- *     value: 'foo/bar',
- *   });
+ * const myDefault = new BlockquoteBaseMeta({
+ *   type: 'one',
+ *   key: 'basic',
+ *   value: 'foo/bar',
+ * });
  *
- * console.log(myDefault.mapList); // {{ id: 'dsfaskj0' }: 'foo/bar'}
+ * console.log(myDefault.objectList); // {basic: 'foo/bar'}
+ * ```
+ *
+ * #### MapList Getter - Map
+ *
+ * ```js
+ * import { BlockquoteBaseMeta } from '@blockquote-web-components/blockquote-base-meta';
+ *
+ * const keyInfo = { id: 'dsfaskj0' };
+ * const myDefault = new BlockquoteBaseMeta({
+ *   type: 'two',
+ *   key: keyInfo,
+ *   value: 'foo/bar',
+ * });
+ *
+ * console.log(myDefault.mapList); // Map(1) { { id: 'dsfaskj0' } => 'foo/bar' }
  * ```
  */
 export class BlockquoteBaseMeta {
@@ -94,18 +121,18 @@ export class BlockquoteBaseMeta {
    *   value?: *
    * }} [options]
    */
-  constructor(options) {
+  constructor({ type = 'default', key, value } = {}) {
     /**
      * Type of Meta
      */
-    this.type = (options && options.type) || 'default';
+    this.type = type;
 
     /**
      * Key for Meta
      */
-    this.key = options && options.key;
-    if (options && 'value' in options) {
-      this.value = options.value;
+    this.key = key;
+    if (value != null) {
+      this.value = value;
     }
   }
 
@@ -122,32 +149,25 @@ export class BlockquoteBaseMeta {
    * @return {*}
    */
   get value() {
-    const { type } = this;
     const key = this.__key || this.key;
     this.__key = undefined;
-    if (type && key) {
-      return BlockquoteBaseMeta.types.get(type) && BlockquoteBaseMeta.types.get(type).get(key);
-    }
-    return undefined;
+    const typeMap = BlockquoteBaseMeta.types.get(this.type);
+    return typeMap?.get(key);
   }
 
   /**
    * Sets value to instance type and key
    * @param {*} value
    */
+
   set value(value) {
-    const { type } = this;
-    const { key } = this;
-    let __type;
-    if (type && key) {
-      if (BlockquoteBaseMeta.types.get(type) === undefined) {
-        BlockquoteBaseMeta.types.set(type, new Map());
-      }
-      __type = BlockquoteBaseMeta.types.get(type);
-      if (value === null) {
-        __type.delete(key);
+    if (this.type && this.key) {
+      const typeMap = BlockquoteBaseMeta.types.get(this.type) ?? new Map();
+      BlockquoteBaseMeta.types.set(this.type, typeMap);
+      if (value != null) {
+        typeMap.set(this.key, value);
       } else {
-        __type.set(key, value);
+        typeMap.delete(this.key);
       }
     }
   }
@@ -157,12 +177,7 @@ export class BlockquoteBaseMeta {
    * @return {Array}
    */
   get list() {
-    const { type } = this;
-    const itemsType = BlockquoteBaseMeta.types.get(type);
-    if (itemsType) {
-      return Array.from(itemsType.values());
-    }
-    return [];
+    return Array.from(this.mapList.values());
   }
 
   /**
@@ -170,12 +185,7 @@ export class BlockquoteBaseMeta {
    * @return {Map}
    */
   get mapList() {
-    const { type } = this;
-    const itemsType = BlockquoteBaseMeta.types.get(type);
-    if (itemsType) {
-      return itemsType;
-    }
-    return new Map();
+    return BlockquoteBaseMeta.types.get(this.type) ?? new Map();
   }
 
   /**
