1551306c5f920e347f3681d8db1349c08a0d5fe0
oscar marina <oscar7600@outlook.com>
2024-03-09 22:10:08 +0100

    feat(blockquote-tabs): move index to src folder

--- packages/components/blockquote-tabs/index.js
+++
@@ -1,3 +0,0 @@
-export { BlockquoteTabs } from './src/BlockquoteTabs.js';
-export { BlockquoteTab } from './src/tab/BlockquoteTab.js';
-export { BlockquoteTabPanel } from './src/tabpanel/BlockquoteTabPanel.js';
