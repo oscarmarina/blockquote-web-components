0587cb70f5075806c5e1f6622c0c1344964d8d94
oscar marina <oscar7600@outlook.com>
2025-09-14 09:44:39 +0200

    feat(blockquote-controller-context-meta): allow multiple contexts on the same DOM element

--- packages/controllers/blockquote-controller-context-meta/src/index.js
+++ packages/controllers/blockquote-controller-context-meta/src/index.js
@@ -1,7 +1,4 @@
 export {BlockquoteControllerContextMeta} from './BlockquoteControllerContextMeta.js';
 export {BaseContextMetaElement} from './BaseContextMetaElement.js';
 export {contextMetaProvider} from './directives/context-meta-provider.js';
-export {
-  cacheContextMetaProvider,
-  contextMetaKeyProvider,
-} from './directives/cache-context-meta-provider.js';
+export {cacheContextMetaProvider} from './directives/cache-context-meta-provider.js';
